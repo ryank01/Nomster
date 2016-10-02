f8683668db4bc8db796079e8086a86479927773e
Ryan <ryankawall01@gmail.com>
2016-10-02 19:29:03 +0000

    fixed gmail to accept email from app

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "ryankawall01@gmail.com.com",
+    mail(to: "testfirehose@gmail.com",
       subject: "A comment has been added to your place")
   end
 end
